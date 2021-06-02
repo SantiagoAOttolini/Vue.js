21d8c480c19b123c629d3b3f98965a89983d7bc9
santiago <santiago.ottolini97@hotmail.com>
2021-06-02 13:10:45 -0300

    Map state

--- 08 Vuex/components/titleFather.js
+++ 08 Vuex/components/titleFather.js
@@ -2,8 +2,13 @@ Vue.component("Father", {
     template: //html
     `
     <div class="p-5 bg-dark text-white">
-        <h1>number {{$store.state.number}}</h1> 
+        <h1>number {{number}} </h1> 
         <titleChild></titleChild>
     </div>
-    `
+    `,
+    computed: {
+        number(){
+            return store.state.number
+        }
+    }
   });
